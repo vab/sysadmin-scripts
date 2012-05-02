5dee47e72de1618425838c5644afe6dad1a50e3d
V. Alex Brennen <alexbrennen@hushmail.com>
2012-05-02 17:30:53 -0400

    Initial Import

---
+++ sshcachetunnel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# SSH Cache Tunnel Script
+# Author: V. Alex Brennen <vab@mit.edu>
+# License: This script is public domain.
+# Date: 2003-11-01
+
+# Description: This script will tunnel an HTTP cache connection over SSH so
+#	       that you can do off site testing of HTTP IP address access
+#              control/filtering restrictions through an external system
+#	       while on a heavily firewalled LAN with limited exit ports
+#	       allowed. Note: The browser should be configured to use the
+#	       cache as if it was running on the localhost (loopback
+#	       interface).
+
+SSH=/usr/bin/ssh
+SSH_PORT=22
+LOCAL_PROXY_PORT=3128
+REMOTE_PROXY_PORT=3128
+PROXY_ADDR=offsiteproxy.example.com
+
+
+$SSH -p $SSH_PORT -L $LOCAL_PROXY_PORT:localhost:$REMOTE_PROXY_PORT $PROXY_ADDR -f -N
+
