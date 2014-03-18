387b78f5f2e3b51b79c3bf567cd2902d92fae8b8
V. Alex Brennen <vab@mit.edu>
2014-03-18 18:21:50 -0400

    Compresses files with bzip2

---
+++ compress.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Recursive File Compression Script
+# Author: V. Alex Brennen <vab@mit.edu>
+# License: This script is public domain
+# Date: 2014-03-17
+
+# Description: This script will take a directory as an argument.
+# It will then transverse the directory, and all sub directories,
+# and compress the regular files in those directories with bzip2.
+# The script just ignores any files that are not either a regular
+# file or a directory (symbolic links, devices, etc).
+
+BZIP2=/bin/bzip2
+CLVL="-9"
+
+# Test for a server list file argument
+if [ -z "$1" ]; then
+echo "Usage: $0 <directory>";
+exit
+fi
+
+# Make sure the directory is valid and readable
+if [ ! -d "$1" ]; then
+echo "Error: Argument does not appear to be a directory: $1."
+exit
+fi
+
+# This is the fuction that does the actual compression and 
+# directory transversal.
+function compress_files
+{
+	for i in $( ls );
+	do
+		if [ -f $i ]; then
+			$BZIP2 $CLVL $i
+		elif [ -d $i ]; then
+			cd $i
+			compress_files
+			cd ..
+		fi
+	done
+}
+
+# To start the script, we change the current working directory
+# to the directory to compress then kick off the initial call 
+# to the recursive compress_files function.
+cd $1
+compress_files
