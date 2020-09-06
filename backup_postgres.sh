ab35627e89248f0205e6f9d70d4137658c20aab9
V. Alex Brennen <vab@protonmail.com>
2020-09-06 11:56:07 -0400

    code cleanup/err checking

--- backup_postgres.sh
+++ backup_postgres.sh
@@ -1,64 +1,113 @@
 #!/bin/bash
 
 # Shell script to backup Postges databases
-# 
-# This script will create a list of all databases that exist on a 
+#
+# This script will create a list of all databases that exist on a
 # Postgres database server and dump the databases one by one each
 # to its own sql file.
 
 # Author:       V. Alex Brennen <vab@mit.edu>
 # Copyright:    None
-# License:      Public Domain
+# License:      Public domain
 # Created:      2006.10.20
 
 
-# Locations of Programs we'll be using
-PSQL="/usr/bin/psql"
+# Locations of the programs we'll be using.
+# Postgres
 PG_DUMP="/usr/bin/pg_dump"
+PSQL="/usr/bin/psql"
+# General
 BZIP2="/usr/bin/bzip2"
-NICE=/bin/nice
+CHOWN="/usr/bin/chown"
+CHMOD="/usr/bin/chmod"
+DATE="/usr/bin/date"
+EGREP="/usr/bin/egrep"
+HEAD="/usr/bin/head"
+NICE="/bin/nice"
+MKDIR="/usr/bin/mkdir"
+TAIL="/usr/bin/tail"
 
-# The argument to specify a compression level if required
+# The argument to specify a compression level to Bzip2 if required.
 CLVL="-9"
 
-# Use nice
+# Use nice.
 BE_NICE=1
 
-# Nice level
+# Nice level.
 NLVL=19
 
-# Set Database Sever Address
-ADDR="127.0.0.1"
+# Set database sever address.
+DBSRVR="127.0.0.1"
 
-# Text hostname for inclusion in file name
-HOST="$(hostname)"
-
-# Set Username
+# Set username
 USER="postgres"
 
-# Set Password
-#PASS=""
+# Set password.
+# Note:  This variable is exported so that Postgres can pull it from the
+#        environment. Leave this variable commented out (unset) if you
+#        set the PGPASSWORD environment variable somewhere else on your
+#        system.
+#
+#        To make this script more secure, you can leave this variable unset
+#        and use a credentials file located at ~/.pgpass with the format
+#        "hostname:port:database:username:password". For additional
+#        information see:
+#        https://www.postgresql.org/docs/current/libpq-pgpass.html
+#
+#        This script passes Postgres programs the "--no-password" argument to
+#        prevent the program from prompting for a password. If the password is
+#        not available the script will fail cleanly.
+#export PGPASSWORD=""
 
-# Directory to store the backups in
-BACKDIR="/home/backup/postgres"
+# Pipe delimited list of databases to exclude from backups.
+# Note: If you're using AWS RDS you'll likely want to add the database
+#       "rdsadmin" to the exclude list.
+EXCLUDE="template0|template1|postgres"
 
-# Get the date information that we'll use for the backup directories and 
-# to construct the backup file names
-YEAR="$(date +"%Y")"
-MONTH="$(date +"%m")"
-DAY="$(date +"%d")"
+# Directory to store the backups in.
+BACKDIR="/var/backups/postgres"
 
-# Make Sure the back-up directory for the current year exists and is 
+# Backup directory permissions and ownership.
+BACKDIR_PERMS=755
+BACKDIR_GROUP="postgres"
+BACKDIR_USER="postgres"
+
+# Backup permissions.
+BACKUP_PERMS=640
+
+# Should the probem exit on an error dumping a database or continue to
+# attempt to dump any other databases? This is a binary flag.
+CONTINUE_DUMP_ERROR=1
+
+# Get the date information that we'll use for the backup directories and
+# to construct the backup file names.
+YEAR="$($DATE +"%Y")"
+MONTH="$($DATE +"%m")"
+DAY="$($DATE +"%d")"
+
+# Make Sure the back-up directory for the current year exists and is
 # writable by the back-up script.
 BACKDIR="$BACKDIR/$YEAR"
 
 if [ ! -d "$BACKDIR" ]; then
-        /bin/mkdir $BACKDIR
+  $MKDIR $BACKDIR
+  if [ $? -ne 0 ]; then
+    echo "Failed to create to backup directory: $BACKDIR." >&2
+    exit 1
+  fi
 fi
 
 if [ ! -w "$BACKDIR" ]; then
-        /bin/chown postgres:postgres $BACKDIR
-        /bin/chmod 755 $BACKDIR
+  $CHOWN $BACKDIR_USER:$BACKDIR_GROUP $BACKDIR
+  if [ $? -ne 0 ]; then
+    echo "Failed to change ownership on backup directory: $BACKDIR." >&2
+    exit 1
+  fi
+  $CHMOD $BACKDIR_PERMS $BACKDIR
+  if [ $? -ne 0 ]; then
+    echo "Failed to change permissions on backup directory: $BACKDIR." >&2
+    exit 1
+  fi
 fi
 
 # Make sure the back-up directory for the current month exists and is
@@ -66,37 +115,84 @@ fi
 BACKDIR="$BACKDIR/$MONTH"
 
 if [ ! -d "$BACKDIR" ]; then
-        /bin/mkdir $BACKDIR
+  $MKDIR $BACKDIR
+  if [ $? -ne 0 ]; then
+    echo "Failed to create to backup directory: $BACKDIR." >&2
+    exit 1
+  fi
 fi
 
 if [ ! -w "$BACKDIR" ]; then
-        /bin/chown postgres:postgres $BACKDIR
-        /bin/chmod 755 $BACKDIR
+  $CHOWN $BACKDIR_USER:$BACKDIR_GROUP $BACKDIR
+  if [ $? -ne 0 ]; then
+    echo "Failed to change ownership on backup directory: $BACKDIR." >&2
+    exit 1
+  fi
+  $CHMOD $BACKDIR_PERMS $BACKDIR
+  if [ $? -ne 0 ]; then
+    echo "Failed to change permissions on backup directory: $BACKDIR." >&2
+    exit 1
+  fi
 fi
 
 # Construct the date to be used in the backup filename.
 DATE="$YEAR$MONTH$DAY"
 
-# Go to the back up directory
+# Go to the backup directory.
 cd $BACKDIR
+if [ $? -ne 0 ]; then
+  echo "Failed change to backup directory: $BACKDIR." >&2
+  exit 1
+fi
 
-# Get a list of all the dbs
-DBS="$($PSQL -U $USER -l -t | awk '{ print $1}' )"
+# Get a list of all the dbs that are not included in the EXCLUDE variable.
+DBS="$($PSQL -h $DBSRVR -U $USER --no-password -c "SELECT datname FROM pg_database;" postgres | $TAIL -n +3 | $HEAD -n -2 | $EGREP -v $EXCLUDE)"
+if [ $? -ne 0 ]; then
+  echo "Failed to get a list of databases." >&2
+  exit 1
+fi
 
-# Iterate through the database list
-for db in $DBS
+# Iterate through the database list.
+for DB in $DBS
 do
-	if [ "$db" != "template0" ] && [ "$db" != "template1" ]; then
-		# set the filename
-		FILE="$db.$HOST.$DATE.sql"
+  # Set the filename.
+  FILE="$DB.$DBSRVR.$DATE.sql"
+  BACKUP_FILE="$DB.$DBSRVR.$DATE.sql.bz2"
 
-		# dump the data
-		$PG_DUMP --create --oids -U $USER -f $FILE $db
-		if [ $BE_NICE -eq 1 ]; then
-			$NICE -$NLVL $BZIP2 $CLVL $FILE
-		else
-			$BZIP2 $CLVL $FILE
-		fi
-	fi
+  # Dump the data.
+  $PG_DUMP -h $DBSRVR -U $USER --no-password --create --oids -U $USER -f $FILE $DB
+  if [ $? -ne 0 ]; then
+    echo "Failed to dump $DB." >&2
+    if [ $CONTINUE_DUMP_ERROR -eq 0 ]; then
+      exit 1
+    fi
+  fi
+
+  # Compress the dump file.
+  if [ $BE_NICE -eq 1 ]; then
+    $NICE -$NLVL $BZIP2 $CLVL $FILE
+    if [ $? -ne 0 ]; then
+      echo "Failed to compress $DB." >&2
+      if [ $CONTINUE_DUMP_ERROR -eq 0 ]; then
+        exit 1
+      fi
+    fi
+  else
+    $BZIP2 $CLVL $FILE
+    if [ $? -ne 0 ]; then
+      echo "Failed to compress $DB." >&2
+      if [ $CONTINUE_DUMP_ERROR -eq 0 ]; then
+        exit 1
+      fi
+    fi
+  fi
+
+  # Set the file permissions on the backup file.
+  $CHMOD $BACKUP_PERMS $BACKUP_FILE
+  if [ $? -ne 0 ]; then
+    echo "Failed to change permissions on the backup file: $BACKFILE." >&2
+    if [ $CONTINUE_DUMP_ERROR -eq 0 ]; then
+      exit 1
+    fi
+  fi
 done
-
